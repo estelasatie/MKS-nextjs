fccea0d9f06122b6b04189aa5f746c29c211f634
Estela Satie <estela.tatie@hotmail.com>
2024-05-08 22:06:43 -0300

    create next.js project

---
+++ app/cart.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import  Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <Head>
+      <title>Carrinho</title>
+      <meta name="description" content="Meu carrinho" />
+      <link rel="icon" href="/favcon.ico" />
+    </Head>
+
+    <main>
+      <h1>
+       Carrinho
+      </h1>
+    </main>
+    </>
+  )
+}
+
+export default Home
---
+++ app/products.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import  Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <Head>
+      <title>Conheça nossos produtos</title>
+      <meta name="description" content="Conheça todos os nossos produtos" />
+      <link rel="icon" href="/favcon.ico" />
+    </Head>
+
+    <main>
+      <h1>
+       Produtos
+      </h1>
+    </main>
+    </>
+  )
+}
+
+export default Home
